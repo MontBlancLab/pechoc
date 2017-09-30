0d5c8fd02e78e775ffee3c3c1b3388c08b4a98d8
Pierre-Gilles Levallois <pgl@erasme.org>
2017-09-30 16:49:39 +0200

    déroulement de l'oeuf ok

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ server.listen( port, function( ) {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/img', express.static(__dirname + '/img')); 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
