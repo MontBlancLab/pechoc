a4230c5039a7694f60dc2ced080096a4a76e7081
Pierre-Gilles Levallois <pgl@erasme.org>
2017-09-30 15:37:43 +0200

    routing with ejs templating

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+var port = 5000;
+var path = require('path');
 var express = require('express');
-
 var app = express();
 
+var server = require('http').createServer(app);
+var data  = null;
+
+//---------------------------------------------------------------------------
+server.listen( port, function( ) {		
+  console.log( 'it is listening at port %d', port );
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
 app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Pecho un chocard, l\'agence matrimoniale des chorcards !');
+
+	//var html = new EJS({url: 'index.ejs'}).render(data);
+	res.render('index',data)
+
+    //res.setHeader('Content-Type', 'text/html');
 });
 
 app.get('/naissance', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('ici l\'oeuf');
+
+	res.render('naissance',data)
+	//var html = new EJS({url: 'naissance.ejs'}).render(data);
+    //res.end('ici l\'oeuf');
 });
 
 app.get('/tinder', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Ici tinder');
+
+	res.render('tinder',data)
+	//var html = new EJS({url: 'tinder.ejs'}).render(data);
+ 	//res.render('index', { data: data });
+
+    //res.end('Ici tinder');
 });
 
 app.listen(8080);
